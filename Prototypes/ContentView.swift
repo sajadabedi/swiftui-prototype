b321d61aba66492a930d556f446c7b3555fead7b
Sajjad Abedi <sajadxabedi@gmail.com>
2024-12-11 14:48:30 +0200

    Initial Commit

---
+++ Prototypes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Prototypes
+//
+//  Created by Sajjad Abedi on 11.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Prototypes/PrototypesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrototypesApp.swift
+//  Prototypes
+//
+//  Created by Sajjad Abedi on 11.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct PrototypesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
