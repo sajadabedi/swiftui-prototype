f2621b4492f3a6645e63e3f5945c96c35589c021
Sajjad Abedi <sajadxabedi@gmail.com>
2024-12-11 14:52:23 +0200

    init commit

---
+++ Prototypes/MeshGradientButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  MeshGradientButtonView.swift
+//  Prototypes
+//
+//  Created by Sajjad Abedi on 11.12.2024.
+//
+
+import SwiftUI
+
+struct MeshGradientButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeshGradientButtonView()
+}
---
+++ Prototypes/RoundedSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  RoundedSheetView.swift
+//  Prototypes
+//
+//  Created by Sajjad Abedi on 11.12.2024.
+//
+
+import SwiftUI
+
+struct RoundedSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoundedSheetView()
+}
